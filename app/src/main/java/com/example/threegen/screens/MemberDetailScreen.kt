5a4677556f0eec810bd92c0527e150f7782bd03f
yoges <yogesh_bharuch@yahoo.co.in>
2025-03-15 11:03:15 +0530

    commited test comment

--- app/src/main/java/com/example/threegen/screens/MemberDetailScreen.kt
+++ app/src/main/java/com/example/threegen/screens/MemberDetailScreen.kt
@@ -55,7 +55,6 @@ fun MemberDetailScreen(
         //Log.d("MemberDetailScreen", "Calling fetchMemberDetails for ID from launched effect : $memberState")
         viewModel.fetchMemberDetails(memberId)
     }
-
     when (val state = memberState) {
         is MemberState.Loading -> LoadingState()
         is MemberState.Empty -> EmptyState() //SuccessList
@@ -106,7 +105,7 @@ fun MemberDetailScreen(
                                     viewModel.updateMember(memberId = member.id, firstName = editableMember.value.firstName, middleName = editableMember.value.middleName ?: "", lastName = editableMember.value.lastName, town = editableMember.value.town, parentID = editableMember.value.parentID, spouseID = editableMember.value.spouseID, imageUri = editableMember.value.imageUri, childNumber = editableMember.value.childNumber, comment = editableMember.value.comment)
                                     Toast.makeText(context, "Member details saved!", Toast.LENGTH_SHORT).show()
                                 },
-                                modifier = Modifier.weight(1f)
+                                modifier = Modifier.weight(1
                             ) { Text(text = "Save") }
 
                             Spacer(modifier = Modifier.width(8.dp))
